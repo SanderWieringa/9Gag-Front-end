6a9a9663329d2d11b5baeb81ecce739c41a6e834
SanderWieringa <s.wieringa@student.fontys.nl>
2023-06-15 02:55:45 +0200

    local url

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
                 //http://34.141.52.221.nip.io/api/Post
                 //https://localhost:44329/api/Post
                 //http://http://acmegag.com/api/Post
-                const response = await fetch('http://34.141.52.221.nip.io/api/Post');
+                const response = await fetch('https://localhost:44329/api/Post');
                 if (response.ok) {
                     const jsonData = await response.json();
                     setData(jsonData);
